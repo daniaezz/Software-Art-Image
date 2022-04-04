c896699ad75279fba9260dfcea5a0d27be40773a
dan <90758857+daniaezz@users.noreply.github.com>
2022-04-04 17:06:39 +0400

    nicer still no gui

--- lines/src/ofApp.cpp
+++ lines/src/ofApp.cpp
@@ -2,7 +2,10 @@
 
 //--------------------------------------------------------------
 void ofApp::setup(){
-
+    ofBackground(255);
+    gui.setup();
+    gui.add(intSlider.setup("int slider", 64, 3, 64));
+    gui.add(floatSlider.setup("float slider", 30.0, 0.0, 300.0));
 }
 
 //--------------------------------------------------------------
@@ -15,18 +18,21 @@ void ofApp::draw(){
     float time = ofGetElapsedTimef();
     float yey = 0.05;
     float xex = 0.03;
+    ofSetLineWidth(8);
 //    ofDrawLine(100, 100, 100, 300);
-    for (int i =0; i< 20; i++){
+    for (int i = 0; i < 50; i++){
+
         float yconst = ofNoise(yey*time);
         float xconst = ofNoise(xex*time);
-//        ofDrawCircle(ofGetWidth()*ofNoise(time*0.3*(i+1)/2), ofGetHeight()* ofNoise(time*0.2*(i+1)/2), 50);
+        ofSetColor(ofNoise(time)*300, yconst*300, xconst*300, 80);
+        //        ofDrawCircle(mouseX, mouseY, 50);
         ofDrawLine(xconst*ofGetWidth(),ofGetHeight()*ofNoise(time*0.3*(i+1)/2), ofGetWidth()* ofNoise(time*0.2*(i+1)/2),yconst*ofGetHeight() );
     }
 }
 
 //--------------------------------------------------------------
 void ofApp::keyPressed(int key){
-
+    
 }
 
 //--------------------------------------------------------------
@@ -46,7 +52,7 @@ void ofApp::mouseDragged(int x, int y, int button){
 
 //--------------------------------------------------------------
 void ofApp::mousePressed(int x, int y, int button){
-
+//    ofDrawCircle(mouseX, mouseY, 50);
 }
 
 //--------------------------------------------------------------
--- lines/src/ofApp.h
+++ lines/src/ofApp.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include "ofMain.h"
+#include "ofxGui.h"
 
 class ofApp : public ofBaseApp{
 
@@ -20,5 +21,23 @@ class ofApp : public ofBaseApp{
 		void windowResized(int w, int h);
 		void dragEvent(ofDragInfo dragInfo);
 		void gotMessage(ofMessage msg);
+    
 		
+    ofxPanel gui;
+    
+    ofxIntSlider intSlider;
+    ofxFloatSlider floatSlider;
+    
+    ofxToggle toggle;
+    ofxButton button;
+    ofxLabel label;
+    
+    ofxIntField intField;
+    ofxFloatField floatField;
+    ofxTextField textField;
+    
+    ofxVec2Slider vec2Slider;
+    ofxVec3Slider vec3Slider;
+    ofxVec4Slider vec4Slider;
+    
 };
