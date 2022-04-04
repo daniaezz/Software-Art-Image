abab8c39137ff949b8088db5224dd9aaf1d1eb9e
dan <90758857+daniaezz@users.noreply.github.com>
2022-04-04 11:55:59 +0400

    why is it making a slant lmao
    10 lines moving with noise

--- lines/src/ofApp.cpp
+++ lines/src/ofApp.cpp
@@ -12,7 +12,16 @@ void ofApp::update(){
 
 //--------------------------------------------------------------
 void ofApp::draw(){
-
+    float time = ofGetElapsedTimef();
+    float heh = 0.05;
+    
+//    ofDrawLine(100, 100, 100, 300);
+    for (int i =0; i< 10; i++){
+        float yconst = ofNoise(heh);
+//        ofDrawCircle(ofGetWidth()*ofNoise(time*0.3*(i+1)/2), ofGetHeight()* ofNoise(time*0.2*(i+1)/2), 50);
+        ofDrawLine(ofGetWidth()*ofNoise(time*0.3*(i+1)/2), ofGetHeight()*ofNoise(time*0.3*(i+1)/2), ofGetWidth()* ofNoise(time*0.2*(i+1)/2), yconst);
+        heh = heh+ 0.005;
+    }
 }
 
 //--------------------------------------------------------------
